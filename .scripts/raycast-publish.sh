6c7c3335fc19a1188444f49dec89084b0711b6fe
Robert Lin <robert@bobheadxi.dev>
2022-01-18 22:14:15 -0800

    chore: revamp publish script

---
+++ .scripts/raycast-publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RAYCAST_EXTENSIONS_DIR=${RAYCAST_EXTENSIONS_DIR:-"../../raycast/extensions"}
+EXTENSION_VERSION=$(git rev-parse --short HEAD)
+
+echo "Publish $EXTENSION_VERSION to $RAYCAST_EXTENSIONS_DIR"
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit
+rm -rf "$RAYCAST_EXTENSIONS_DIR/extensions/sourcegraph"
+rsync -rv --exclude=.git --exclude=.gitignore --exclude=node_modules --exclude=.scripts \
+    ./ "$RAYCAST_EXTENSIONS_DIR/extensions/sourcegraph"
+
+pushd "$RAYCAST_EXTENSIONS_DIR" || exit
+git add .
+git status
+git commit -m "Publish raycast-sourcegraph@$EXTENSION_VERSION" -m "Version: https://github.com/bobheadxi/raycast-sourcegraph/commit/$EXTENSION_VERSION"
+popd || exit
