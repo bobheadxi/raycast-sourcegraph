39b165d40958c005efa79e2dc2dc035733f22e63
Robert Lin <robert@bobheadxi.dev>
2024-12-06 14:35:33 -0800

    telemetry: generate and preserve anonymous user ID if no token is present

--- src/findNotebooksInstance.tsx
+++ src/findNotebooksInstance.tsx
@@ -1,6 +1,6 @@
 import FindNotebooksCommand from "./components/FindNotebooksCommand";
 import InstanceCommand from "./components/InstanceCommand";
 
-export default function FindNotebooksSelfHosted() {
+export default async function FindNotebooksSelfHosted() {
   return <InstanceCommand Command={FindNotebooksCommand} />;
 }
