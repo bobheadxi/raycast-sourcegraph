0baced28deaf498d53350fd911e60d5f99a3e98d
Robert Lin <robert@bobheadxi.dev>
2025-06-29 08:52:57 -0700

    tools: fix formatting

--- src/tools/shared/fileread.ts
+++ src/tools/shared/fileread.ts
@@ -17,10 +17,11 @@ export async function executeFileRead(
     const abortController = new AbortController();
 
     // Use doQuery to get file contents
-    const contentArgs = startLine !== undefined || endLine !== undefined
-      ? `(startLine: ${startLine || 0}, endLine: ${endLine || -1})`
-      : '';
-    
+    const contentArgs =
+      startLine !== undefined || endLine !== undefined
+        ? `(startLine: ${startLine || 0}, endLine: ${endLine || -1})`
+        : "";
+
     const query = `{
       repository(name: ${JSON.stringify(repository)}) {
         id
